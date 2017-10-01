5bd6c27eccdd76532288d30eae74368d33f02772
Luana Catao <luana.ctrentin@gmail.com>
2017-10-01 09:32:54 -0300

    exemplo1.1

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-	/* //Far to Cel for fahr = 0, 20, ..,300
+	 //Far to Cel for fahr = 0, 20, ..,300
 
 	float fahr, celsius;
 	int lower, upper, step;
@@ -13,29 +13,15 @@ int main()
 
 	fahr = lower;
 	printf(" Fahrenheit to Celsius \n\n");
-	while (fahr <= upper)
-	{ celsius = 5.0/9.0 * (fahr-32);
-	  printf("%3.3f %3.3f\n" , fahr , celsius);
-	  fahr = fahr + step;
-
-	} */
-
-	float fahr, celsius;
-	int lower, upper, step;
-
-	lower = 0;
-	upper = 200;
-	step = 30;
-
-	celsius = lower;
-	printf(" Celsius to Fahrenheit \n\n");
-
-	while (celsius <= upper)
-	{
-		fahr = 32 + 9.0/5.0 * celsius;
-		printf("%3.3f \t %3.3f\n" , celsius , fahr);
-		celsius = celsius + step;
+	while (fahr <= upper) {
+		celsius = 5.0/9.0 * (fahr-32);
+		printf("%3.3f %3.3f\n" , fahr , celsius);
+		fahr = fahr + step;
 
 	}
+
+
+
+
 }
 
